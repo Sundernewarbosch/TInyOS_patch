3177a822b68d8d2ffa349a7d021c1f1462104c45
FIXED-TERM Newar Sunder (BGSW/PJ-ETA-P) <exw1kor@PUA-C-0007P.kor.apac.bosch.com>
2025-10-30 15:01:51 +0530

    remove_PartnerBookmarksProvider

--- packages/services/Car/car_product/build/car_system.mk
+++ packages/services/Car/car_product/build/car_system.mk
@@ -32,7 +32,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Applications
 PRODUCT_PACKAGES += \
-    PartnerBookmarksProvider \
     preinstalled-packages-platform-generic-system.xml \
     Stk \
     Tag \
