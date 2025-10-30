9fafc05f24cf6171be9ea8651cb2f51baa276c9e
FIXED-TERM Newar Sunder (BGSW/PJ-ETA-P) <exw1kor@PUA-C-0007P.kor.apac.bosch.com>
2025-10-30 11:35:59 +0530

    remove_CalendarProvider

--- build/target/product/handheld_system.mk
+++ build/target/product/handheld_system.mk
@@ -38,7 +38,6 @@ PRODUCT_PACKAGES += \
     BlockedNumberProvider \
     BluetoothMidiService \
     BookmarkProvider \
-    CalendarProvider \
     cameraserver \
     CameraExtensionsProxy \
     CaptivePortalLogin \
